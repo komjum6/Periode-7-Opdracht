a95b73c2c1c3363d55318e17c17da95049242c44
Unknown <jphh56@gmail.com>
2018-02-12 16:14:16 +0100

    (Python) Versie 0.3 (Pre-Alpha)

--- Python ORF Finder/ORF+Finder.py
+++ Python ORF Finder/ORF+Finder.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[41]:
+# In[28]:
 
 from tkinter import filedialog
 from tkinter import ttk
@@ -22,8 +22,8 @@ class ORF:
 class ORFGUI(Frame):  
     def __init__(self, master):
         try:
-            seq = StringVar()
-            aminoAcidMap = {
+            seq = StringVar() #De sequentie als string
+            aminoAcidMap = {  #Een aminozuurdictionary
                 'TTT' : 'F',
                 'TTC' : 'F',
                 'TTA' : 'L',
@@ -112,10 +112,10 @@ class ORFGUI(Frame):
             StopCodons = ['TAG', 'TGA', 'TAA']
 
             def load_file():
-                orflijst = []
-                Filename = self.fileName = filedialog.askopenfilename(filetypes = (("My Files","*.fasta"),("All Files","*.*")),title = "Choose a Fasta File")
-                file = open(Filename, "r")
-                print("Your Filename is: ", Filename, "\n")
+                orflijst = [] #Dit is een lijst waar alle ORF objecten (eigenlijk nog gewoon DNA en headers...) in komen
+                Filename = self.fileName = filedialog.askopenfilename(filetypes = (("My Files","*.fasta"),("All Files","*.*")),title = "Choose a Fasta File") #Dit maakt een Filechooser
+                file = open(Filename, "r") #Openen van de file
+                print("Your Filename is: ", Filename, "\n") #Printen van de naam van de file
             
                 yconcat = "" 
                 headerlist = []
@@ -208,45 +208,56 @@ class ORFGUI(Frame):
                 
                 #ranges = [(n, min(n+1, 3)) for n in range(1, 3)]
                 #print("This is first index: ",ranges)
+                
                 features = []
+                displayString = ""
                 count = 0
                 for x in range(0,len(result)):
                     found_orf = seqje[slice(*result[count])] #Door de index te veranderen is door de ORF's te bladeren
                     print("This is ",count," index: ",found_orf)
-                    color = "#%06x" % random.randint(0, 0xFFFFFF)
-                    features.append(GraphicFeature(start=result[count][0], end=result[count][1], strand=+1, color=color, label= str(count) + " Index ORF", labelcolor=color))
-                    print("result count ",*result[count])
+                    displayString = displayString + str(count) + " index " + found_orf + "\n" #Een string maken met alle ORF's
+                    color = "#%06x" % random.randint(0, 0xFFFFFF) #Een willekeurige kleur wordt gekozen
+                    features.append(GraphicFeature(start=result[count][0], end=result[count][1], strand=+1, color=color, label= str(count) + " Index ORF", labelcolor=color)) #Het toevoegen van de entries aan het figuur
+                    print("result count ",*result[count]) #Print de start en stoppositie van de ORF's
                     count += 1
-                
-                record = GraphicRecord(sequence_length=len(seqje), features=features)
-                record.plot(figure_width=15)
+                    
+                self.text.insert(END, displayString) #De orf's als strings laten zien in de GUI
+                record = GraphicRecord(sequence_length=len(seqje), features=features) #Het maken van een figuur met de ORF's
+                record.plot(figure_width=15) #Het figuur wordt gemaakt, de grootte van het figuur valt aan te passen
                 seq.set(str(result))
                 print(seq.get())
 
+            #Het maken van de master, het hoofdscherm
+            self.master = master #De master is het hoofdscherm
+            master.title("ORF Predictor") #De titel van het hoofdscherm
+            self.button = Button(master, text="Load File and find ORF's", command = lambda:[load_file(),findOpenReadingFrames(seq.get())]) #Een button met text en twee functies
+            self.button.bind("<Button-1>") #De button vertellen wat voor actie het activeerd            
+            self.button.grid(row=0) #De button toevoegen aan het 'grid', rij 0
+            self.label = Label(master, text ="ORF IN DNA",foreground="red",font=("Helvetica", 16)) #Het maken van een label
+            self.label.grid(row=1) #De label toevoegen aan het 'grid', rij 1
             
-            self.master = master
-            master.title("ORF Predictor")
-            self.frame = Frame(master, width=400, height=400)
-            self.frame.grid(row=0)
-            self.button = Button(master, text="Load File and find ORF's", command = lambda:[load_file(),findOpenReadingFrames(seq.get())])
-            self.button.bind("<Button-1>")            
-            self.button.grid(row=1)
-            self.label = Label(master, text ="ORF IN DNA",foreground="red",font=("Helvetica", 16))
-            self.label.grid(row=2)
-            progressbar = ttk.Progressbar(master, orient='horizontal', mode='determinate')
-            progressbar.grid(row=3,padx=5, pady=5) #Dit heeft een waarde tussen 0 en 100
+            #progressbar = ttk.Progressbar(master, orient='horizontal', mode='determinate')
+            #progressbar.grid(row=3,padx=5, pady=5) #Dit heeft een waarde tussen 0 en 100
             
-            root.mainloop()  
+            self.frame = Frame(master, width=400, height=400) #Dit wordt toch veranderd, dus 400 x 400 is onnodig
+            self.frame.grid(row=2) #Het frame toevoegen aan het 'grid', rij 2
+            self.text = Text(self.frame, height=6, width=30) #Het maken van een textblok in het frame
+            self.text.grid(row=2, column=0, sticky="nsew", padx=5, pady=5) #Het textblok toevoegen aan het 'grid', rij 2, in het frame
+            self.scrollbar = Scrollbar(self.frame, orient="vertical", command = self.text.yview) #Het maken van een scrollbar
+            self.scrollbar.grid(row=2, column=1, sticky="nsew") #De scrollbar toevoegen aan het 'grid', rij 2, in het frame
+            self.text['yscrollcommand'] = self.scrollbar.set #De scrollbar een commando meegeven
+            
+            root.mainloop() #Het uitvoeren van de mainloop
             
             
             
         
         except TclError:
-            print("TclError")
+            print("TclError") #Deze error komt als je .pack() en .grid() door elkaar gebruikt
         
     def __del__(self):
         class_name = self.__class__.__name__
-        print(class_name, "destroyed")
+        print(class_name, "destroyed") #Dit is het bericht dat komt als de GUI is afgesloten
           
     
 if __name__ == "__main__":
