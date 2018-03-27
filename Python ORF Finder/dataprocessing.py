2279eedf1055fa1e8ae377bd4223e2a33aa3d57a
Justin <jphh56@gmail.com>
2018-03-27 13:12:21 +0200

    Versie 0.42 (Pre-Alpha)

--- Python ORF Finder/dataprocessing.py
+++ Python ORF Finder/dataprocessing.py
@@ -3,7 +3,7 @@ from tkinter import END
 import random
 import matplotlib.pyplot as plt
 
-aminoAcidMap = {  #Een aminozuurdictionary
+aminoAcidMap = {                #Een aminozuurdictionary
                 'TTT' : 'F',
                 'TTC' : 'F',
                 'TTA' : 'L',
@@ -87,56 +87,56 @@ aminoAcidMap = {  #Een aminozuurdictionary
                 'GGA' : 'G',
                 'GGG' : 'G',
     
-'ANC' : 'X', #Ik heb hier wat toegevoegd om N's af te vangen. Het lukt maar niet.
-'CNA' : 'X',
-'NCA' : 'X',
-'NCN' : 'X',
-'TCN' : 'X',
-'TNC' : 'X',
-'NGT' : 'X',
-'ANG' : 'X',
-'GNA' : 'X',
-'ANN' : 'X',
-'NGA' : 'X',
-'NNA' : 'X',
-'GNT' : 'X',
-'CTN' : 'X',
-'CNG' : 'X',
-'CGN' : 'X',
-'NAC' : 'X',
-'GNN' : 'X',
-'GAN' : 'X',
-'TAN' : 'X',
-'NNG' : 'X',
-'NNT' : 'X',
-'ACN' : 'X',
-'CNT' : 'X',
-'AGN' : 'X',
-'NGN' : 'X',
-'GCN' : 'X',
-'NAG' : 'X',
-'CNN' : 'X',
-'NGC' : 'X',
-'GTN' : 'X',
-'TNN' : 'X',
-'NCT' : 'X',
-'NTA' : 'X',
-'TGN' : 'X',
-'NNC' : 'X',
-'ANT' : 'X',
-'GNC' : 'X',
-'NAT' : 'X',
-'NTG' : 'X',
-'NAN' : 'X',
-'CAN' : 'X',
-'NTC' : 'X',
-'TNG' : 'X',
-'NTN' : 'X',
-'TNA' : 'X',
-'ATN' : 'X',
-'NCG' : 'X',
-'NNN' : 'X'
-} #Belangrijke toevoeging
+                'ANC' : 'X', #Hier is wat toegevoegd om N's af te vangen.
+                'CNA' : 'X',
+                'NCA' : 'X',
+                'NCN' : 'X',
+                'TCN' : 'X',
+                'TNC' : 'X',
+                'NGT' : 'X',
+                'ANG' : 'X',
+                'GNA' : 'X',
+                'ANN' : 'X',
+                'NGA' : 'X',
+                'NNA' : 'X',
+                'GNT' : 'X',
+                'CTN' : 'X',
+                'CNG' : 'X',
+                'CGN' : 'X',
+                'NAC' : 'X',
+                'GNN' : 'X',
+                'GAN' : 'X',
+                'TAN' : 'X',
+                'NNG' : 'X',
+                'NNT' : 'X',
+                'ACN' : 'X',
+                'CNT' : 'X',
+                'AGN' : 'X',
+                'NGN' : 'X',
+                'GCN' : 'X',
+                'NAG' : 'X',
+                'CNN' : 'X',
+                'NGC' : 'X',
+                'GTN' : 'X',
+                'TNN' : 'X',
+                'NCT' : 'X',
+                'NTA' : 'X',
+                'TGN' : 'X',
+                'NNC' : 'X',
+                'ANT' : 'X',
+                'GNC' : 'X',
+                'NAT' : 'X',
+                'NTG' : 'X',
+                'NAN' : 'X',
+                'CAN' : 'X',
+                'NTC' : 'X',
+                'TNG' : 'X',
+                'NTN' : 'X',
+                'TNA' : 'X',
+                'ATN' : 'X',
+                'NCG' : 'X',
+                'NNN' : 'X'
+                } 
 
 StartCodon = 'ATG'
 
@@ -144,15 +144,15 @@ StopCodons = ['TAG', 'TGA', 'TAA']
 
 #-----------------------------------------Begin van de Functies, hieronder zijn functies die in andere functies worden gebruikt---------------------------------------------
 
-def findStartPositions(SEQ):
+def findStartPositions(seq):
     # uppercase seq
-    SEQ = SEQ.upper()
+    seq = seq.upper()
     # maak een lijst voor de resultaten
     resultstart = []
     # loop over alle indeces behalve de laatste twee want daar kunnen geen codons starten
-    for i in range(len(SEQ)-2):
+    for i in range(len(seq)-2):
         # kijk of het codon een startcodon is
-        if SEQ[i:i+3] == StartCodon:
+        if seq[i:i+3] == StartCodon:
             # append de startcodon positie bij de lijst
             resultstart.append(i)
     # Retourneer de lijst
@@ -189,17 +189,9 @@ def findNextStopCodon(seq, start):
         # return None als er geen stop condons zijn
         return None
     
-#-----------------------------------------Functie voor het verkrijgen van de radioInt voor verwerking-----------------------------------------------------------------    
-#def retrieveRadioInt(self, radioInt = radioInt.get()):
-    #if radioInt == None:
-        #return None
-    #else:
-        #return radioInt
-    #, retrieveRadioInt(self, radioInt.get())
-    
 #-----------------------------------------Hieronder zijn functies voor het maken van de data van de plots-------------------------------------------------------------
     
-def findOpenReadingFrames(self, seq, seqje, Frameskew = -3, alternative_result = []):
+def findOpenReadingFrames(self, seqje, Frameskew = -3, alternative_result = []):
     
     radioInt = self.radioInt.get() #Hier is het begin van radioInt in de Middle Tier (buiten de GUI)
     
@@ -237,77 +229,72 @@ def findOpenReadingFrames(self, seq, seqje, Frameskew = -3, alternative_result =
     #Roep de volgende functies aan, de variabele Frameskew mag niet groter zijn dan 2
     if Frameskew != 3:
         if radioInt == 1 or radioInt == 2:
-            findOpenReadingFrames(self, seq, seqje, Frameskew +1) #Recursie
-            printOpenReadingFrames(self, seq, seqje, result, Frameskew, radioInt) #Gaat verder met de data in een volgende functie
+            findOpenReadingFrames(self, seqje, Frameskew +1) #Recursie
+            printOpenReadingFrames(self, seqje, result, Frameskew, radioInt) #Gaat verder met de data in een volgende functie
         if radioInt == 0: 
-            findOpenReadingFrames(self, seq, seqje, Frameskew +1) #Recursie
+            findOpenReadingFrames(self, seqje, Frameskew +1) #Recursie
             if Frameskew == 2:
-                printOpenReadingFramesOneFigure(self, seq, seqje, alternative_result, Frameskew, radioInt) #Gaat verder met de data in een volgende functie
+                printOpenReadingFramesOneFigure(self, seqje, alternative_result, Frameskew, radioInt) #Gaat verder met de data in een volgende functie
     
-def printOpenReadingFrames(self, seq, seqje, result, Frameskew, radioInt, recordlijst = [], Frameskewlijst = [], MaxAanroep = 0):
-    
-    MaxAanroep += 1
-    if MaxAanroep % 6 != 0:
-    
-        #Er is geen Reading Frame 0
-        if Frameskew >=0:
-            Frameskew += 1
-
-        features = []
-        displayString = ""
-        count = 0
-        for x in range(0,len(result)):
-            found_orf = seqje[slice(*result[count])].replace("\n","") #Door de index te veranderen is door de ORF's te bladeren, de replace was nodig vanwege de irritante \n's
-            #print("This is ",count," index: ",found_orf)
-
-            skew = 0 #Dit is een lelijke manier om Aminozuren te verkrijgen, beide als code en omdat er 1 of wat meer nucleotiden worden weggegooid
-            if len(found_orf) % 3 == 1: 
-                skew = 1
-            if len(found_orf) % 3 == 2:
-                skew = 2
-
-            displayString = displayString + "\n" + " Index " + str(count) + " Reading Frame " + str(Frameskew) + "\n" + "  ORF in Nucleotides" + "\n" + found_orf + "\n" + "  ORF in Amino Acids" + "\n" + "".join([aminoAcidMap[found_orf[x:x+3]] for x in range(0, (len(found_orf)-skew), 3)]) + "\n" #Een string maken met alle ORF's
-            color = "#%06x" % random.randint(0, 0xFFFFFF) #Een willekeurige kleur wordt gekozen
-            features.append(GraphicFeature(start=result[count][0], end=result[count][1], strand=+1, color=color, label= str(count) + " Index ORF", labelcolor=color)) #Het toevoegen van de entries aan het figuur
-            #print("result count ",*result[count]) #Print de start en stoppositie van de ORF's
-            count += 1
-
-        self.text.insert(END, displayString) #De orf's als strings laten zien in de GUI
-        record = GraphicRecord(sequence_length=len(seqje), features=features) #Het maken van een figuur met de ORF's
-
-        recordlijst.append(record) #Vul de lijst met recordobjecten
-        Frameskewlijst.append(str(Frameskew)) #Maak een string lijst met de Reading Frames voor de namen van de figuren
-        
-        print(len(recordlijst))
-        
-        if len(recordlijst) == 6: #Er zijn altijd maar 6 Reading Frames mogelijk
-            plotOpenReadingFrames(self, recordlijst, Frameskewlijst, radioInt) #Aanroepen van de volgende functie
-    else:
-        recordlijst = []
-        Frameskewlijst = []
-
-#-----------------------------------------Een alternatieve functie voor het maken van de data van de plots-------------------------------------------
-        
-def printOpenReadingFramesOneFigure(self, seq, seqje, alternative_result, Frameskew, radioInt, recordlijst = [], Frameskewlijst = []): #Op het moment wordt deze functie nergens aangeroepen
+def printOpenReadingFrames(self, seqje, result, Frameskew, radioInt, recordlijst = [], Frameskewlijst = []):
     
     #Er is geen Reading Frame 0
     if Frameskew >=0:
         Frameskew += 1
-    
+
     features = []
     displayString = ""
     count = 0
+    for x in range(0,len(result)):
+        found_orf = seqje[slice(*result[count])].replace("\n","") #Door de index te veranderen is door de ORF's te bladeren, de replace was nodig vanwege de irritante \n's
+        #print("This is ",count," index: ",found_orf)
+
+        skew = 0 #Dit is een lelijke manier om Aminozuren te verkrijgen, beide als code en omdat er 1 of wat meer nucleotiden worden weggegooid
+        if len(found_orf) % 3 == 1: 
+            skew = 1
+        if len(found_orf) % 3 == 2:
+            skew = 2
+
+        displayString = displayString + "\n" + " Index " + str(count) + " Reading Frame " + str(Frameskew) + "\n" + "  ORF in Nucleotides" + "\n" + found_orf + "\n" + "  ORF in Amino Acids" + "\n" + "".join([aminoAcidMap[found_orf[x:x+3]] for x in range(0, (len(found_orf)-skew), 3)]) + "\n" #Een string maken met alle ORF's
+        color = "#%06x" % random.randint(0, 0xFFFFFF) #Een willekeurige kleur wordt gekozen
+        features.append(GraphicFeature(start=result[count][0], end=result[count][1], strand=+1, color=color, label= str(count) + " Index ORF", labelcolor=color)) #Het toevoegen van de entries aan het figuur
+        #print("result count ",*result[count]) #Print de start en stoppositie van de ORF's
+        count += 1
+
+    self.text.insert(END, displayString) #De orf's als strings laten zien in de GUI
+    record = GraphicRecord(sequence_length=len(seqje), features=features) #Het maken van een figuur met de ORF's
+
+    recordlijst.append(record) #Vul de lijst met recordobjecten
+    Frameskewlijst.append(str(Frameskew)) #Maak een string lijst met de Reading Frames voor de namen van de figuren
+
+    print(len(recordlijst))
+
+    if len(recordlijst) == 6: #Er zijn altijd maar 6 Reading Frames mogelijk
+        plotOpenReadingFrames(self, recordlijst, Frameskewlijst, radioInt) #Aanroepen van de volgende functie
+        
+
+#-----------------------------------------Een alternatieve functie voor het maken van de data van de plots-------------------------------------------
+        
+def printOpenReadingFramesOneFigure(self, seqje, alternative_result, Frameskew, radioInt, recordlijst = [], Frameskewlijst = []): 
     
+    #Er is geen Reading Frame 0
+    if Frameskew >=0:
+        Frameskew += 1
+
+    features = []
+    displayString = ""
+    count = 0
+
     for x in range(0,len(alternative_result)):
         found_orf = seqje[slice(*alternative_result[count])].replace("\n","") #Door de index te veranderen is door de ORF's te bladeren, de replace was nodig vanwege de irritante \n's
         #print("This is ",count," index: ",found_orf)
-        
+
         #skew = 0 #Dit is een lelijke manier om Aminozuren te verkrijgen, beide als code en omdat er 1 of nucleotiden worden weggegooid
         #if len(found_orf) % 3 == 1: 
         #    skew = 1
         #if len(found_orf) % 3 == 2:
         #    skew = 2
-        
+
         #displayString = displayString + "\n" + " Index " + str(count) + " Reading Frame " + str(Frameskew) + "\n" + "  ORF in Nucleotides" + "\n" + found_orf + "\n" + "  ORF in Amino Acids" + "\n" + "".join([aminoAcidMap[found_orf[x:x+3]] for x in range(0, (len(found_orf)-skew), 3)]) + "\n" #Een string maken met alle ORF's
         color = "#%06x" % random.randint(0, 0xFFFFFF) #Een willekeurige kleur wordt gekozen
         features.append(GraphicFeature(start=alternative_result[count][0], end=alternative_result[count][1], strand=+1, color=color, label= str(count) + " Index ORF", labelcolor=color)) #Het toevoegen van de entries aan het figuur
@@ -316,10 +303,10 @@ def printOpenReadingFramesOneFigure(self, seq, seqje, alternative_result, Frames
 
     #self.text.insert(END, displayString) #De orf's als strings laten zien in de GUI
     record = GraphicRecord(sequence_length=len(seqje), features=features) #Het maken van een figuur met de ORF's
-    
+
     recordlijst.append(record) #Vul de lijst met recordobjecten
     Frameskewlijst.append(str(Frameskew)) #Maak een string lijst met de Reading Frames voor de namen van de figuren
-    
+
     plotOpenReadingFrames(self, recordlijst, Frameskewlijst, radioInt) #Aanroepen van de volgende functie
     
         
@@ -356,8 +343,16 @@ def plotOpenReadingFrames(self, recordlijst, Frameskewlijst, radioInt):
             plt.show() #De aanpassingsfase is over (zoals het zetten van de titel etc), nu worden de figuren getoont
     
 #-----------------------------------------Garbage code, DO NOT DELETE (may be useful later)-------------------------------------------------------------        
+    """
+    seq.set(str(result))
+    print(seq.get())
     
-    #seq.set(str(result))
-    #print(seq.get())
+    MaxAanroep.append(0)
+    print("Maxaanroep begin" ,MaxAanroep)
+    if len(MaxAanroep) % 6 != 0:
+        print("Maxaanroep" ,MaxAanroep)
+    else:
+    recordlijst = []
+    Frameskewlijst = []
     
-    
+    """
