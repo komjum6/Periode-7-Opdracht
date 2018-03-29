c5970138dc64c089760379b74e9a1218d7571ef4
Justin <jphh56@gmail.com>
2018-03-29 16:05:41 +0200

    Bestandsmap hernoemd

--- Python ORF Finder/LocalBlast.bat
+++
@@ -1,7 +0,0 @@
-cd C:\Users\Luukv\Desktop\ORF finder\Periode-7-Opdracht\Python ORF Finder
-move ORF.txt C:\BLAST\blast-2.7.1+\bin
-cd C:\BLAST\blast-2.7.1+\bin
-makeblastdb -in ORF.txt -dbtype nucl -out Local
-tblastn -db Local -query Genes.txt -evalue 0.00001 -outfmt 5 -out Result.txt
-Result.txt
-move Result.txt C:\Users\Luukv\Desktop\ORF finder\Periode-7-Opdracht\Python ORF Finder
