db583cf3e59f160c0b8e27da6af7bb79c657feec
Justin <jphh56@gmail.com>
2018-03-29 16:09:33 +0200

    Weer een vieze conflict

--- Python ORF Finder/LocalBlast.bat
+++
@@ -1,7 +0,0 @@
-cd D:\School\HAN\2017?2018 (Bi2c)\Course 7\Tutor\Periode-7-Opdracht\Python ORF Finder
-move ORF.txt C:\Program Files\NCBI\blast-2.7.1+\bin
-cd C:\Program Files\NCBI\blast-2.7.1+\bin
-makeblastdb -in ORF.txt -dbtype nucl -out Local
-tblastn -db Local -query Genes.txt -evalue 0.00001 -outfmt 5 -out Result.txt
-Result.txt
-move Result.txt D:\School\HAN\2017?2018 (Bi2c)\Course 7\Tutor\Periode-7-Opdracht\Python ORF Finder
