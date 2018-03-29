6622897460521ec576928ea7eb850ff92111e8d4
Unknown <luukvdamme@gmail.com>
2018-03-29 15:25:20 +0200

    versie 0.54(Pre_Alpha)

--- Python_ORF_Finder/LocalBlast.bat
+++ Python_ORF_Finder/LocalBlast.bat
@@ -1,7 +1,16 @@
-cd D:\School\HAN\2017?2018 (Bi2c)\Course 7\Tutor\Periode-7-Opdracht\Python ORF Finder
+cd C:\Users\Luukv\Desktop\ORF_Finder\Periode-7-Opdracht\Python_ORF_Finder
+move Genes.txt C:\Program Files\NCBI\blast-2.7.1+\bin
+
 move ORF.txt C:\Program Files\NCBI\blast-2.7.1+\bin
+
 cd C:\Program Files\NCBI\blast-2.7.1+\bin
+
 makeblastdb -in ORF.txt -dbtype nucl -out Local
-tblastn -db Local -query Genes.txt -evalue 0.00001 -outfmt 5 -out Result.txt
+
+tblastn -db Local -query Genes.txt -outfmt 5 -out Result.txt
+
 Result.txt
-move Result.txt D:\School\HAN\2017?2018 (Bi2c)\Course 7\Tutor\Periode-7-Opdracht\Python ORF Finder
+
+move ORF.txt C:\Users\Luukv\Desktop\ORF_Finder\Periode-7-Opdracht\Python_ORF_Finder
+move Result.txt C:\Users\Luukv\Desktop\ORF_Finder\Periode-7-Opdracht\Python_ORF_Finder
+move Genes.txt C:\Users\Luukv\Desktop\ORF_Finder\Periode-7-Opdracht\Python_ORF_Finder
